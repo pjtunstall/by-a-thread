ffff7443389c9d0dc4f6acb501527cea1d59129f
pjtunstall <pjtunstall@gmail.com>
2025-12-01 19:09:10 +0000

    Extract 'magic numbers' into named consts

--- client/src/player.rs
+++ client/src/player.rs
@@ -2,9 +2,11 @@ use macroquad::prelude::*;
 
 use shared::player::{Player, PlayerInput};
 
+pub const INPUT_HISTORY_LENGTH: usize = 256;
+
 pub struct ClientPlayer {
     pub shared: Player,
     pub interpolation_start: Vec3,
     pub prediction_age: u32,
-    pub input_history: [PlayerInput; 256],
+    pub input_history: [PlayerInput; INPUT_HISTORY_LENGTH],
 }
--- server/src/player.rs
+++ server/src/player.rs
@@ -1,8 +1,10 @@
 use shared::player::{Player, PlayerInput};
 
+pub const INPUT_BUFFER_LENGTH: usize = 128;
+
 pub struct ServerPlayer {
     pub shared: Player,
     pub last_processed_tick: u64,
     pub last_input: Option<PlayerInput>,
-    pub input_buffer: [PlayerInput; 128],
+    pub input_buffer: [PlayerInput; INPUT_BUFFER_LENGTH],
 }
