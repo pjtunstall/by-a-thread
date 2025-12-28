1b4f5be7417b78b6b1ba95626834c53c193afa9c
pjtunstall <pjtunstall@gmail.com>
2025-12-28 20:13:35 +0000

    Server Game state receives player input

--- server/src/state_handlers/game.rs
+++ server/src/state_handlers/game.rs
@@ -4,15 +4,25 @@ use crate::{
     net::ServerNetworkHandle,
     state::{Game, ServerState},
 };
-use common::{net::AppChannel, player::PlayerInput};
+use common::{net::AppChannel, protocol::ClientMessage};
 
 pub fn handle(network: &mut dyn ServerNetworkHandle, _state: &mut Game) -> Option<ServerState> {
     for client_id in network.clients_id() {
         while let Some(data) = network.receive_message(client_id, AppChannel::Unreliable) {
-            let decoded = decode_from_slice::<PlayerInput, _>(&data, standard());
-            if let Ok((input, _)) = decoded {
-                // TODO: Pass PlayerInput to this player's update method.
-                let _ = input;
+            let Ok((message, _)) = decode_from_slice::<ClientMessage, _>(&data, standard()) else {
+                eprintln!("Client {} sent malformed data. Disconnecting.", client_id);
+                network.disconnect(client_id);
+                continue;
+            };
+
+            match message {
+                ClientMessage::Input(input) => {
+                    // TODO
+                }
+
+                _ => {
+                    continue;
+                }
             }
         }
     }
