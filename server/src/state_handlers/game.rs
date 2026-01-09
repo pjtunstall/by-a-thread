b63e5cbf828d8999c00c0c7e300209c93bea0a4c
pjtunstall <pjtunstall@gmail.com>
2026-01-09 22:03:27 +0000

    Minor edit

--- server/src/state_handlers/game.rs
+++ server/src/state_handlers/game.rs
@@ -4,16 +4,16 @@ use crate::{
     state::{Game, ServerState},
 };
 
-// TODO:
-// - Have the server increment its tick.
-// - Process inputs for current tick.
-// - Send customized snapshot to each player.
-// (See also the `Game` struct in `server/src/state.rs`.)
-
 // TODO: If connection times out during game (and elsewhere), show a suitable
 // message in the UI; currently it just goes black.
 pub fn handle(network: &mut dyn ServerNetworkHandle, state: &mut Game) -> Option<ServerState> {
     input::receive_inputs(network, state);
+
+    // TODO:
+    // - Process inputs for current tick.
+    // - Send customized snapshot to each player.
+    // - Have the server increment its tick.
+    // (See also the `Game` struct in `server/src/state.rs`.)
 
     None
 }
