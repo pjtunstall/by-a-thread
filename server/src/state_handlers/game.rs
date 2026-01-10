9a2e38e9bc4560b5a964fd1c52e6d49b0a4ced9c
pjtunstall <pjtunstall@gmail.com>
2026-01-10 13:16:01 +0000

    Minor edit

--- server/src/state_handlers/game.rs
+++ server/src/state_handlers/game.rs
@@ -11,33 +11,20 @@ use crate::{
 pub fn handle(network: &mut dyn ServerNetworkHandle, state: &mut Game) -> Option<ServerState> {
     input::receive_inputs(network, state);
 
-    let n = state.players.len();
-    for i in 0..n {
-        let input_option = state.players[i].input_buffer.get(state.current_tick);
-        let input = match input_option {
-            Some(&input) => {
-                state.players[i].last_input = input;
-                input
-            }
-            None => {
-                // println!(
-                //     "Mismatched input ids for player '{}'; falling back to last known input",
-                //     state.players[i].name
-                // );
-                state.players[i].last_input
-            }
-        };
+    for player in &mut state.players {
+        if let Some(&input) = player.input_buffer.get(state.current_tick) {
+            player.last_input = input;
+        }
 
-        println!("{:?}", input);
+        let input = player.last_input;
+        // println!("{:?}", input);
 
-        state.players[i].state.update(&state.maze, &input);
-        state.players[i]
-            .input_buffer
-            .advance_tail(state.current_tick);
+        player.state.update(&state.maze, &input);
+        player.input_buffer.advance_tail(state.current_tick);
     }
 
-    for i in 0..n {
-        let snapshot = state.snapshot_for(i);
+    for i in 0..state.players.len() {
+        let _snapshot = state.snapshot_for(i);
 
         // TODO: Send customized snapshot to each player: their own velocity and
         // everyone's position.
