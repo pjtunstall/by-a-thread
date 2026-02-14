795a60c73d3dc675b60c1a777bb2b88b52d3583e
pjtunstall <pjtunstall@gmail.com>
2026-02-14 15:05:46 +0000

    Add to comment on what to do in the event of system clock not being synced

--- common/src/time.rs
+++ common/src/time.rs
@@ -1,14 +1,25 @@
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+// If either of these functions panics because the system time is before the
+// Unix epoch, try to enable automatic time synchronization (NTP) as follows:
+//
+// Linux: `timedatectl set-ntp true` (or `rc-service ntpd start` / `rc-service
+// chronyd start` on non-systemd distros).
+//
+// Windows: Settings -> Time & language -> Date & time, enable 'Set time
+// automatically'.
+//
+// macOS: System Settings -> General -> Date & Time, enable 'Set date and time
+// automatically'.
 pub fn now() -> Duration {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .expect("system time is before unix epoch") // If this problem occurs, open system's date and time settings and enable automatic time synchronization (NTP). On most Linux systems, try `timedatectl set-ntp true`. On non-systemd distros (like Alpine or Gentoo), use `rc-service ntpd start` or `rc-service chronyd start` instead.
+        .expect("system time is before Unix epoch")
 }
 
 pub fn now_as_secs_f64() -> f64 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .expect("time went backwards")
+        .expect("system time is before Unix epoch")
         .as_secs_f64()
 }
