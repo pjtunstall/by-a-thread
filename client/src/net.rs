983fc8a66625aed06f6d06a8feb3062450b21f7f
pjtunstall <pjtunstall@gmail.com>
2026-02-14 12:16:35 +0000

    Change token validity to 23 minutes

--- client/src/net.rs
+++ client/src/net.rs
@@ -26,7 +26,7 @@ pub fn create_connect_token(
     ConnectToken::generate(
         current_time,
         protocol_id,
-        1380, // Valid for 23 minutes.
+        23 * 60, // Valid for 23 minutes.
         client_id,
         15, // Timeout after 15 seconds.
         vec![server_addr],
