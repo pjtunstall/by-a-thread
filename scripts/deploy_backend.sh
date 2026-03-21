ac20d77bdb965e31c130e924d58c91479f5daf53
pjtunstall <pjtunstall@gmail.com>
2026-03-21 15:40:20 +0000

    Note extra VPS config in deploy_backend.sh comments

--- scripts/deploy_backend.sh
+++ scripts/deploy_backend.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# Before this script runs,
+
+# 1.Make sure that Docker is installed and configured to
+# restart after a reboot:
+# sudo systemctl enable docker.service
+#sudo systemctl enable containerd.service
+
+# 2. Run `sudo visudo` and add
+# `non-root-user ALL=(ALL) NOPASSWD: /usr/sbin/reboot`
+# to the end of the sudoers file.
+
+# 3. Run `sudo systemctl edit docker.service` and make sure that the following
+#    lines are present and uncommented so that Docker only starts after the
+# network is online and the time is set:
+# After=network-online.target nss-lookup.target docker.socket containerd.service time-set.target
+# Wants=network-online.target containerd.service
+
 set -euo pipefail
 
 REPO_OWNER="pjtunstall"
@@ -17,9 +34,6 @@ docker compose --env-file .env.matchmaker up -d --remove-orphans
 
 docker system prune -af
 
-# Before this script runs, run `sudo visudo`
-# and add `non-root-user ALL=(ALL) NOPASSWD: /usr/sbin/reboot`
-# to the end of the sudoers file.
 if [ -f /var/run/reboot-required ]; then
     sudo /usr/sbin/reboot
 fi
